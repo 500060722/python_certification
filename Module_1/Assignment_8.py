7e2d7b71141fb6319cef15fafbf7f2caa76e435d
Prajjawal Banati <500060722@stu.upes.ac.in>
2018-06-01 01:45:27 -0700

    Adding files in to the repo

---
+++ Module_1/Assignment_8.py
@@ -0,0 +1,7 @@
+bill_id=int(input("Enter the bill_id of the person"))
+customer_id=int(input("Enter the customer_id of the person"))
+if_minor=False
+bill_amount=float(input("Enter the bill_amount of the customer"))
+print("THE BILL_ID OF THE PERSON IS",bill_id)
+print("THE CUSTOMER_ID OF THE PERSON IS",customer_id)
+print("THE BILL_AMOUNT OF THE PERSON IS",bill_amount)
